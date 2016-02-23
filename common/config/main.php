da03c6900405fc9abfd3e9300292047d23513481
499478785 <499478785@qq.com>
2016-02-23 10:10:36 +0800

    a

--- common/config/main.php
+++ common/config/main.php
@@ -8,9 +8,9 @@ return [
         // ...
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=jm', // MySQL, MariaDB
-            'username' => 'root', //数据库用户名
-            'password' => '', //数据库密码
+            'dsn' => 'mysql:host=114.80.78.241;dbname=bhy', // MySQL, MariaDB
+            'username' => 'newbhy', //数据库用户名
+            'password' => 'newbhy*2016', //数据库密码
             'charset' => 'utf8',
         ],
     ],
