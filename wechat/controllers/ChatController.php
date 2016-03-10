5687fa3aaaf85346d7bc69481e569e74ea85f881
499478785 <499478785@qq.com>
2016-03-10 15:45:15 +0800

    tes

--- wechat/controllers/ChatController.php
+++ wechat/controllers/ChatController.php
@@ -3,18 +3,15 @@
  * Created by PhpStorm.
  * User: Administrator
  * Date: 2016/3/10
- * Time: 12:05
+ * Time: 15:45
  */
 
 namespace wechat\controllers;
 
 
-use wechat\controllers\BaseController;
-
-class ChatController extends BaseController{
+class ChatController {
 
     public function actionIndex(){
-
         return $this->render();
     }
 }
