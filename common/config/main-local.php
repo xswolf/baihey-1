069032bf9ba443ebf4db8107d8ea552bd3ecc944
499478785 <499478785@qq.com>
2016-03-14 11:12:44 +0800

    聊天

---
+++ common/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=jm',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'tablePrefix' => 'bhy_',   //加入前缀名称fc_
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
