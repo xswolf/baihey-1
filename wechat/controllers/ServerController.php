2eb1638bce2816bb714fe8ff6db0d0cbd427f5cc
499478785 <499478785@qq.com>
2016-03-15 15:54:50 +0800

    聊天

---
+++ wechat/controllers/ServerController.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/3/15
+ * Time: 15:45
+ */
+
+namespace wechat\controllers;
+
+
+class ServerController extends BaseController {
+
+    /**
+     * 微信服务器推送事件
+     */
+    public function actionEvent() {
+
+        if ( ! isset( $_GET['echostr'] ) ) {
+            // 关注
+            $this->responseMsg();
+        } else {
+            // 验证服务器
+            $this->validate();
+        }
+    }
+
+    private function validate() {
+        $echoStr = $_GET["echostr"];
+        echo $echoStr;
+    }
+
+    private function responseMsg() {
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+        //extract post data
+        if ( ! empty( $postStr ) ) {
+            /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+               the best way is to check the validity of xml by yourself */
+            libxml_disable_entity_loader( true );
+            $postObj      = simplexml_load_string( $postStr , 'SimpleXMLElement' , LIBXML_NOCDATA );
+            $fromUsername = $postObj->FromUserName;
+            $toUsername   = $postObj->ToUserName;
+            $keyword      = trim( $postObj->Content );
+            $time         = time();
+            $textTpl      = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>";
+
+            $msgType    = "text";
+            $contentStr = "Welcome to wechat world!";
+            $resultStr  = sprintf( $textTpl , $fromUsername , $toUsername , $time , $msgType , $contentStr );
+            echo $resultStr;
+        } else {
+            echo "";
+            exit;
+        }
+    }
+
+}
