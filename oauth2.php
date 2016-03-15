2281bc7f9ab640a106ad5b5c7ab94803ec732fb4
499478785 <499478785@qq.com>
2016-03-15 10:39:57 +0800

    聊天

---
+++ oauth2.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/3/15
+ * Time: 10:38
+ */
+if (isset($_GET['code'])){
+    echo $_GET['code'];
+}else{
+    echo "NO CODE";
+}
