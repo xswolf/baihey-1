937e0025e14a79fcda3749618885117f94011af5
499478785 <499478785@qq.com>
2016-03-16 13:24:28 +0800

    聊天

--- wechat/web/js/app.js
+++ wechat/web/js/app.js
@@ -21,15 +21,18 @@ requirejs(['jquery', './chat', 'comm','jweixin'], function ($, chat, comm,wx) {
     wx.config(config);
     console.log(1);
     wx.ready(function(){
-        wx.chooseImage({
-            count: 1, // 默认9
-            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-            success: function (res) {
-                var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-            }
-        });
+
     });
+
+    wx.chooseImage({
+        count: 1, // 默认9
+        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+        success: function (res) {
+            var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+        }
+    });
+
     console.log(2);
 
     wx.error(function(res){
