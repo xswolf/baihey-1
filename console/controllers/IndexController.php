67e0ef8e74eee38a5ae146b7809d45773c567779
499478785 <499478785@qq.com>
2016-03-18 10:52:56 +0800

    聊天

---
+++ console/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+namespace console\controllers;
+use yii\base\Controller;
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/3/18
+ * Time: 10:41
+ */
+class IndexController extends Controller{
+
+    public function actionIndex(){
+        echo 123;
+    }
+}
---
+++ console/controllers/ServerSocketController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/3/18
+ * Time: 10:51
+ */
+
+namespace console\controllers;
+
+
+use yii\base\Controller;
+
+class ServerSocketController extends Controller{
+
+    public function actionRun(){
+        echo 'this socket start';
+    }
+}
