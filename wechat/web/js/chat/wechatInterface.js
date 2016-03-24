5d2fc076c5440bc8b73563c03d4394f945d396cc
sg <825512123@qq.com>
2016-03-24 16:33:56 +0800

    图片上传

--- wechat/web/js/chat/wechatInterface.js
+++ wechat/web/js/chat/wechatInterface.js
@@ -54,7 +54,7 @@ define(['http://res.wx.qq.com/open/js/jweixin-1.0.0.js','chat/chat'] , function 
             sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
             sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
             success: function (res) {
-                localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                localId = res.localId; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
                 if (localId != null) {
                     wx.uploadImage({
                         localId: localId, // 需要上传的图片的本地ID，由chooseImage接口获得
