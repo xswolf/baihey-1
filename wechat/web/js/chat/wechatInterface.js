438e80323bdec285ae0c559f878c314bf9cbe78f
499478785 <499478785@qq.com>
2016-03-24 12:01:33 +0800

    聊天

--- wechat/web/js/chat/wechatInterface.js
+++ wechat/web/js/chat/wechatInterface.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2016/3/23.
  */
-define(['http://res.wx.qq.com/open/js/jweixin-1.0.0.js','chat/chat'] , function (wx,chat) {
+define(['http://res.wx.qq.com/open/js/jweixin-1.0.0.js'] , function (wx) {
     // 微信接口调用
     wx.setConfig = function ($config) {
 
@@ -28,7 +28,6 @@ define(['http://res.wx.qq.com/open/js/jweixin-1.0.0.js','chat/chat'] , function 
                     if (localId != null) {
                         wx.uploadVoice({
                             localId: localId, // 需要上传的音频的本地ID，由stopRecord接口获得
-                            isShowProgressTips: 1, // 默认为1，显示进度提示
                             success: function (res) {
                                 serverId = res.serverId; // 返回音频的服务器端ID
                             }
