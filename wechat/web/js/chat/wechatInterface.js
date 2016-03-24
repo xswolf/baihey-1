c62f0ab67073713a86752443ca3f61d449d621d0
sg <825512123@qq.com>
2016-03-24 16:24:32 +0800

    图片上传

--- wechat/web/js/chat/wechatInterface.js
+++ wechat/web/js/chat/wechatInterface.js
@@ -47,6 +47,28 @@ define(['http://res.wx.qq.com/open/js/jweixin-1.0.0.js','chat/chat'] , function 
      * @param toUser
      */
     wx.send_pic = function(toUser){
+        var localId = null;
+        var serverId = null; // 图片服务端ID
+        wx.chooseImage({
+            count: 1, // 默认9
+            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+            success: function (res) {
+                localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                if (localId != null) {
+                    wx.uploadImage({
+                        localId: localId, // 需要上传的图片的本地ID，由chooseImage接口获得
+                        isShowProgressTips: 1, // 默认为1，显示进度提示
+                        success: function (res) {
+                            serverId = res.serverId; // 返回图片的服务器端ID
+                            chat.sendMessage(serverId, toUser,'record')
+                        }
+                    });
+                } else {
+                    alert('没有图片Id');
+                }
+            }
+        });
 
     }
 
