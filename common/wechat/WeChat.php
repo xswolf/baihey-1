8d6923aa7401d292f7bdf6542a096d0c3230cbe9
sg <825512123@qq.com>
2016-03-28 18:27:16 +0800

    测试

--- common/wechat/WeChat.php
+++ common/wechat/WeChat.php
@@ -17,7 +17,7 @@ class WeChat extends \callmez\wechat\sdk\Wechat{
         $secret = \Yii::$app->wechat->appSecret;
         $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appid&secret=$secret";
         $html = file_get_contents($url);
-        var_dump($html);exit;
+        var_dump(json_decode($html));exit;
         return $html['access_token'];
     }
 
