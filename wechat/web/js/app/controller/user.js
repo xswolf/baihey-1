50dee5bc3fa73a857f7c2149f24df048dd114674
Administrator <570434734@qq.com>
2016-03-29 14:59:24 +0800

    s

--- wechat/web/js/app/controller/user.js
+++ wechat/web/js/app/controller/user.js
@@ -2,44 +2,61 @@
  * Created by Administrator on 2016/3/22.
  */
 
-define(['jquery','angular','app/module','app/directive/directiveApi'
-,'app/service/serviceApi'
-], function ($,angular,module) {
-    module.controller("login" , ['app.serviceApi','$scope',function (api,$scope ) {
+define(['jquery', 'angular', 'app/module', 'app/directive/directiveApi'
+    , 'app/service/serviceApi'
+], function ($, angular, module) {
+    module.controller("login", ['app.serviceApi', '$scope', function (api, $scope) {
 
         //设定页面高度为屏幕高度
         $scope.winHeight = {
-            'height': document.documentElement.clientHeight+ 'px'
+            'height': document.documentElement.clientHeight + 'px'
         }
 
         //获取验证码
-        $scope.getCode = function(){
+        $scope.getCode = function () {
 
-             if(!ar.validateMobile($scope.mobile)){   //验证手机号码格式
-                 $('#bhy-alert1').modal();
-                 return false;
-             }
+            /*if (!ar.validateMobile($scope.mobile)) {   //验证手机号码格式
+                $('#bhy-alert1').modal();
+                return false;
+            }*/
+            $scope.codeSwitch = true;
+            console.log($scope.getCodeBtn);
+            $scope.$apply();
 
-             /*if(!api.getMobileIsExist($scope.mobile)){
+            /* if (!api.getMobileIsExist($scope.mobile)) { //验证手机是否已存在
                  $('#bhy-alert2').modal();
                  return false;
-             }
-*/
-            //计时
+             }*/
+
+            /*//计时
             var btn = $('.getCode');
+            btn.prop('disabled', true).addClass('dslb');
             max_time = 60;
             var timer = setInterval(function () {
-                btn_tick(btn,max_time);
+                btn_tick(btn, max_time);
             }, 1000);
             setTimeout(function () {
-                //btn.prop('disabled', false).removeClass('dslb');
-                $scope.codeSwitch = true;
-                console.log($scope.codeSwitch);
-                end_tick(btn,timer);
-            }, max_time * 1000);
-
+                btn.prop('disabled', false).removeClass('dslb');
+                end_tick(btn, timer);
+            }, max_time * 1000);*/
 
         }
+
+        $scope.register = function () {
+            if (!api.validateCode($scope.code)) {
+                $('#bhy-alert3').modal();
+                return false;
+            }
+            $scope.formData = {'sex': $scope.sex, 'mobile': $scope.mobile}; //组装表单数据
+            var result = api.save($scope.formData);
+            if (result.status == 1) {  //注册成功
+                window.location.href = '';
+            } else {                   //注册失败
+                $('#bhy-alert4').modal();
+                return false;
+            }
+        }
+
 
     }])
 
@@ -51,7 +68,8 @@ function btn_tick(btn) {
     btn.val("重新发送" + max_time);
 }
 
-function end_tick(btn,timerObj) {
+function end_tick(btn, timerObj) {
     btn.val("获取验证码");
     clearInterval(timerObj);
 }
+
--- wechat/web/js/app/service/serviceApi.js
+++ wechat/web/js/app/service/serviceApi.js
@@ -1,8 +1,8 @@
 /**
  * Created by Administrator on 2016/3/23.
  */
-define(['app/module'] , function (module) {
-    module.factory('app.serviceApi' , ['$http' , function ($http) {
+define(['app/module'], function (module) {
+    module.factory('app.serviceApi', ['$http', function ($http) {
 
         var api = {};
 
@@ -15,10 +15,32 @@ define(['app/module'] , function (module) {
             return $http.get('url....');
         }
 
-        api.getMobileIsExist = function(mobile){
-            return $http.get('url',{params:{mobile:mobile}});
+        /**
+         * 验证手机号是否存在
+         * @param mobile
+         * @returns {*}
+         */
+        api.getMobileIsExist = function (mobile) {
+            return $http.get('url', {params: {mobile: mobile}});
         }
 
+        /**
+         * 验证短信验证码
+         * @param code
+         * @returns {*}
+         */
+        api.validateCode = function (code) {
+            return $http.get('url',{params:{code:code}});
+        }
+
+        /**
+         * 注册
+         * @param formData
+         * @returns {*}
+         */
+        api.save = function (formData) {
+            return $http.post('url',{params:{data:formData}});
+        }
 
         api.wxConfig = function (wx ) {
 
