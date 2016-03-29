c259d93fa0716e84ec3dd6ca6dd2b146ca89d1be
sg <825512123@qq.com>
2016-03-29 10:50:29 +0800

    测试

--- common/wechat/WeChat.php
+++ common/wechat/WeChat.php
@@ -13,7 +13,7 @@ class WeChat extends \callmez\wechat\sdk\Wechat{
     }
 
     public function getAccessToken() {
-        var_dump(\Yii::$app);
+        var_dump(\Yii::$app->vendorPath);
         /*$accessToken = \Yii::$app->wechat->getAccessToken();
         var_dump($accessToken);*/exit;
         return $accessToken['access_token'];
