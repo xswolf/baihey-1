5dd8e2020df0308365f50aaf4c3132e817a19f61
499478785 <499478785@qq.com>
2016-04-01 14:51:00 +0800

    聊天

--- wechat/controllers/ServerController.php
+++ wechat/controllers/ServerController.php
@@ -46,12 +46,19 @@ class ServerController extends BaseController {
             $fromUsername = $postObj->FromUserName;
             $fromUsername = trim($fromUsername);
             $toUsername   = $postObj->ToUserName;
+            $time         = time();
+            $textTpl      = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>";
 
-
-//            $resultStr = \Yii::$app->wechat->responseNews($fromUsername , $toUsername);
-            $resultStr = \Yii::$app->wechat->responseText($fromUsername , $toUsername);
-
-
+            $msgType    = "text";
+            $content = "<a href='http://wechat.baihey.com/wap/chat/chat?name=1&sendName=12'>well come to jia rui</a>";
+            $resultStr  = sprintf( $textTpl , $fromUsername , $toUsername , $time , $msgType , $content );
             $userInfo = \Yii::$app->wechat->getMemberInfo($fromUsername);
             if(is_array($userInfo) && count($userInfo) > 0){
                 file_put_contents('./log.txt' , $fromUsername."\n" ,FILE_APPEND);
@@ -70,9 +77,9 @@ class ServerController extends BaseController {
     }
 
     public function actionMaterial(){
-//        $material = \Yii::$app->wechat->materialList();
+        $material = \Yii::$app->wechat->materialList();
         \Yii::$app->wechat->sendMaterial("oEQpts_MMapxllPTfwRw0VfGeLSg" , "TtSb9HO50njLDfRLrBEM_NKXrzVpIgfX9DYtwftdrGQ");
-//        var_dump($material);
+        var_dump($material);
     }
 
 }
