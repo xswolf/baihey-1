0159f240e49058e33d57027503058d3bf035241d
499478785 <499478785@qq.com>
2016-04-04 12:58:10 +0800

    聊天

--- wechat/config/main.php
+++ wechat/config/main.php
@@ -15,10 +15,10 @@ return [
 
         'wechat' => [
             'class' => 'common\wechat\WeChat',
-//            'appId' => 'wx787f8071dd1e1dac',
-//            'appSecret' => 'd5517695e5b9e5c2b3e119affe8302ad',
-            'appId' => 'wxd7e0ddc236831e27', // 测试
-            'appSecret' => 'e457c991b0fbe7adcec7ed4e6468e20d', // 测试
+            'appId' => 'wx787f8071dd1e1dac',
+            'appSecret' => 'd5517695e5b9e5c2b3e119affe8302ad',
+//            'appId' => 'wxd7e0ddc236831e27', // 测试
+//            'appSecret' => 'e457c991b0fbe7adcec7ed4e6468e20d', // 测试
             'token' => 'jrbhy'
         ],
         'log' => [
--- wechat/controllers/ServerController.php
+++ wechat/controllers/ServerController.php
@@ -64,7 +64,7 @@ class ServerController extends BaseController {
         $result_1 = \Yii::$app->wechat->deleteMenu();
 
         $appId = \Yii::$app->wechat->appId;
-        $redirectUri = urlencode("http://120.76.84.162/wap/chat/list");
+        $redirectUri = urlencode("http://wechat.baihey.com/wap/chat/list");
 
         $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid={$appId}&redirect_uri={$redirectUri}&response_type=code&scope=snsapi_base&state=123#wechat_redirect";
         $result = \Yii::$app->wechat->createMenu([
