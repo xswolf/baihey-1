58cd519fac83981dfaf58620139974b7400d9f1f
Administrator <570434734@qq.com>
2016-04-04 11:29:57 +0800

    s

--- common/message/MessageApi.php
+++ common/message/MessageApi.php
@@ -32,16 +32,14 @@ class MessageApi extends Component{
         $code = rand(100000,999999);
         $nr = '您好，本次验证码为：'.$code.'。【嘉瑞百合缘】';
         $nr = mb_convert_encoding($nr, "GB2312", "UTF-8");
+        session_start();
+        $_SESSION['reg_code'] = $code;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'http://www.106168.net/smsComputer/smsComputersend.asp?zh=cqjrhj&mm=666666&hm='.$phone.'&nr='.$nr.'&dxlbid=19');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         $output = curl_exec($ch);
         curl_close($ch);
-        if($output == '0'){
-            session_start();
-            $_SESSION['reg_code'] = $code;
-        }
         return $output;
     }
 
