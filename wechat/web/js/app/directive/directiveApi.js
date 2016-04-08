a4c727205826ac09c2d961df1a3537967b8b12e4
Administrator <570434734@qq.com>
2016-04-08 11:13:47 +0800

    s

--- wechat/web/js/app/directive/directiveApi.js
+++ wechat/web/js/app/directive/directiveApi.js
@@ -3,45 +3,6 @@
  */
 define(['app/module'], function (module) {
 
-    module.directive("modal", ['api', function (api) {
-        return {
-            restrict: "EA",
-            template: '<div class="am-modal am-modal-no-btn" tabindex="-1" id="bhy-modal"><div class="am-modal-dialog"><div class="am-modal-hd">modal标题<a href="javascript: void(0)" class="am-close am-close-spin" data-am-modal-close>&times;</a> </div> <div class="am-modal-bd">确定</div> </div> </div>'
-        }
-    }]);
-    module.directive("alert", function () {
-        return {
-            restrict: "EA",
-            replace: true,
-            scope: {
-                body: '@',
-                num: '@'
-            },
-            transclude: true,
-            template: '<div class="am-modal am-modal-alert" tabindex="-1" id="bhy-alert{{num}}"><div class="am-modal-dialog"><div class="am-modal-bd" >{{body}}</div><div class="am-modal-footer"><span class="am-modal-btn">确定</span></div></div></div>',
-
-        }
-    });
-    module.directive("confirm", function () {
-        return {
-            restrict: "EA",
-            scope: {
-                body: '@'
-            },
-            template: '<div class="am-modal am-modal-confirm" tabindex="-1" id="my-confirm"> <div class="am-modal-dialog"> <div class="am-modal-bd">{{body}}</div> <div class="am-modal-footer"> <span class="am-modal-btn" data-am-modal-cancel>取消</span> <span class="am-modal-btn" data-am-modal-confirm>确定</span> </div> </div> </div>'
-        }
-    });
-
-    module.directive("modalLoading", function () {
-        return {
-            restrict: "EA",
-            scope: {
-                body: '@'
-            },
-            template: '<div class="am-modal am-modal-loading am-modal-no-btn" tabindex="-1" id="my-modal-loading"> <iv class="am-modal-dialog"> <div class="am-modal-hd">{{body}}</div> <div class="am-modal-bd"> <span class="am-icon-spinner am-icon-spin"></span> </div> </div> </div'
-        }
-    });
-
     module.directive("config", function () {
         return {
             restrict: "E",
@@ -51,11 +12,11 @@ define(['app/module'], function (module) {
             }
         }
     });
-
+    //<input type="text" placeholder={{vm.placeholder}} ng-model="citydata"  class={{vm.cssClass}} readonly>
     module.directive("ionicCityPicker", ['$ionicPopup', '$timeout', 'app.serviceApi', '$ionicScrollDelegate', '$ionicModal', function ($ionicPopup, $timeout, api, $ionicScrollDelegate, $ionicModal) {
         return {
             restrict: 'AE',
-            template: '<input type="text"  placeholder={{vm.placeholder}} ng-model="citydata"  class={{vm.cssClass}} readonly>',
+            template: '<input id="bhy-search-city-picker" readonly class={{vm.cssClass}} ng-model="citydata" value="不限" >',
             scope: {
                 citydata: '=',
                 backdrop: '@',
@@ -68,7 +29,7 @@ define(['app/module'], function (module) {
                 vm.provinceHandle = "provinceHandle" + attrs.citydata;
                 vm.cityHandle = "cityHandle" + attrs.citydata;
                 vm.countryHandle = "countryHandle" + attrs.citydata;
-                vm.placeholder = attrs.placeholder || "不限";
+                //vm.placeholder = attrs.placeholder || "不限";
                 vm.okText = attrs.okText || "确定";
                 vm.cssClass = attrs.cssClass;
                 vm.barCssClass = attrs.barCssClass || "bar-dark";
