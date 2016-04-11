6df4c222dbad2a8026b2ab045cf380cb83a87c0a
499478785 <499478785@qq.com>
2016-04-11 17:01:57 +0800

    wechat

--- wechat/controllers/SiteController.php
+++ wechat/controllers/SiteController.php
@@ -26,25 +26,6 @@ class SiteController extends BaseController
      * 首页列表页
      */
     public function actionUserList(){
-        $where = [
-            'type'=>1,
-            'name'=>['!=' =>'12'],
-            'id'=>['in'=>['1',2,3]],
-            'age'=>['>'=>12],
-            'age1'=>['<'=>12],
-            'age2'=>['>='=>12],
-            'or'=>[
-                'type'=>1,
-                'name'=>['!=' =>'12'],
-                'age3'=>['<='=>12],
-                'age4'=>['between'=>[12,15]],
-            ],
-            'age3'=>['<='=>12],
-            'age4'=>['between'=>[12,15]],
-
-        ];
-        echo User::getInstance()->processWhere($where);
-        exit;
         $list = User::getInstance()->userList();
         $this->renderAjax(['status=>1' , 'data'=>$list] );
     }
