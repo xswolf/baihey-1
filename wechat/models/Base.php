9b7ef111c4c3f7aed8d0c84f2d08451293dbbcca
499478785 <499478785@qq.com>
2016-04-11 17:01:42 +0800

    wechat

--- wechat/models/Base.php
+++ wechat/models/Base.php
@@ -40,32 +40,77 @@ class Base extends \yii\db\ActiveRecord{
 
 
     /**
-     *[
-        'type'=>1,
-        'name'=>['!=' =>'name'],
-        'id'=>['in'=>['1',2,3]],
-        'age'=>['>',12],
-        'age1'=>['<',12],
-        'age2'=>['>=',12],
-        'age3'=>['<=',12],
-        'age3'=>['between',[12,15]],
-        ];
+     *$where = [
+    'type'=>1,
+    'name'=>['!=' =>'12'],
+    'id'=>['in'=>['1',2,3]],
+    'age'=>['>'=>12],
+    'age1'=>['<'=>12],
+    'age2'=>['>='=>12],
+    'age3'=>['<='=>12],
+    'age4'=>['between'=>[12,15]],
+    'or'=>[
+    'type'=>1,
+    'name'=>['!=' =>'12'],
+    'id'=>['in'=>['1',2,3]],
+    'age'=>['>'=>12],
+    'age1'=>['<'=>12],
+    'age2'=>['>='=>12],
+    'age3'=>['<='=>12],
+    'age4'=>['between'=>[12,15]],
+    ]
+    ];
      * @param $where Array
      * @return $where String
      */
-    public function processWhere($where){
-        $where = [
-               'type'=>1,
-               'name'=>['!=' =>'name'],
-                'id'=>['in'=>['1',2,3]],
-                'age'=>['>',12],
-                'age1'=>['<',12],
-                'age2'=>['>=',12],
-                'age3'=>['<=',12],
-                'age3'=>['between',[12,15]],
-              ];
+    public function processWhere($where , $str = 'and'){
 
-        return $where;
+        if (!is_array($where)){
+            return $where;
+        }
+        $sqlWhere='';
+        $i = 1;
+        foreach ($where as $k=>$v){
+            if (!is_array($v)){
+                $sqlWhere .= " {$k} = '{$v}'  {$str} ";
+            }else{
+
+                if ($k == 'or'){
+                    $res = $this->processWhere($v ,'or');
+                    $sqlWhere .= ' (' . $res . ")";
+                }else {
+
+                    foreach ( $v as $subK => $subV ) {
+
+                        switch ( $subK ) {
+                            case '!=':
+                                $sqlWhere .= " {$k} != '{$subV}'  {$str} ";
+                                break;
+
+                            case 'in':
+                                $sqlWhere .= " {$k}  in (" . implode( "," , $subV ) . ")  {$str} ";
+                                break;
+
+                            case 'between':
+
+                                $sqlWhere .= " {$k} between " . "'$subV[0]'" . ' and ' . "'$subV[1]'" . "  {$str} ";
+                                break;
+
+                            default:
+
+                                $sqlWhere .= " {$k} {$subK} '{$subV}'  {$str} ";
+                                break;
+
+                        }
+
+                    }
+                }
+            }
+        }
+
+        $sqlWhere = substr($sqlWhere , 0,-4);
+
+        return $sqlWhere;
     }
 
 
