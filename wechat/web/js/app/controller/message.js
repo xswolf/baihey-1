6da9fad3054d9729686c88969d436a9de835a62f
499478785 <499478785@qq.com>
2016-04-12 15:48:37 +0800

    wechat

--- wechat/web/js/app/controller/message.js
+++ wechat/web/js/app/controller/message.js
@@ -1,51 +1,62 @@
 /**
  * Created by NSK. on 2016/4/5/0005.
  */
-define(['app/module', 'chat/wechatInterface', 'chat/chat', 'app/directive/directiveApi'
+define(['app/module' , 'app/directive/directiveApi'
     , 'app/service/serviceApi', 'comm'
-], function (module, wx, chat) {
+], function (module , wx,chat) {
 
     module.controller("message.index", ['app.serviceApi', '$scope', '$timeout', '$ionicPopup', '$ionicModal', '$ionicActionSheet', '$ionicLoading', function (api, $scope, $timeout, $ionicPopup, $ionicModal, $ionicActionSheet, $ionicLoading) {
 
+        // 加载中动画
+        $ionicLoading.show({
+            content: 'Loading',
+            animation: 'fade-in',
+            showBackdrop: false,
+            maxWidth: 200,
+            showDelay: 0
+        });
+
+        // 模拟延迟2秒展现页面
+        $timeout(function () {
+            $ionicLoading.hide();
+
+            // 获取页面数据
+            $scope.items = [
+                {
+                    id: 1,
+                    name: "张三"
+                },
+                {
+                    id: 2,
+                    name: "李四"
+                },
+                {
+                    id: 3,
+                    name: "王二麻子"
+                },
+                {
+                    id: 4,
+                    name: "王二麻子"
+                },
+                {
+                    id: 5,
+                    name: "王二麻子"
+                },
+                {
+                    id: 6,
+                    name: "王二麻子"
+                },
+                {
+                    id: 7,
+                    name: "王二麻子"
+                }
+
+            ]
+
+            $scope.isFollow = true;   // 是否有谁关注了我，有则显示小红点
 
 
-
-
-        // 获取页面数据
-        $scope.items = [
-            {
-                id: 1,
-                name: "张三"
-            },
-            {
-                id: 2,
-                name: "李四"
-            },
-            {
-                id: 3,
-                name: "王二麻子"
-            },
-            {
-                id: 4,
-                name: "王二麻子"
-            },
-            {
-                id: 5,
-                name: "王二麻子"
-            },
-            {
-                id: 6,
-                name: "王二麻子"
-            },
-            {
-                id: 7,
-                name: "王二麻子"
-            }
-
-        ]
-
-        $scope.isFollow = true;   // 是否有谁关注了我，有则显示小红点
-
+        }, 2000);
 
         // 联系人pop窗口
         $scope.popShow = false;
@@ -60,7 +71,7 @@ define(['app/module', 'chat/wechatInterface', 'chat/chat', 'app/directive/direct
         }
 
         $scope.chatHref = function (id) {
-            window.location.href = '/wap/message/chat?id=' + id;
+            window.location.href = '/wap/message/chat?id='+ id;
         }
 
     }]);
@@ -80,7 +91,7 @@ define(['app/module', 'chat/wechatInterface', 'chat/chat', 'app/directive/direct
 
         //  获取历史聊天数据
         $scope.sendId = ar.getQueryString('id')
-        api.list("/wap/chat/message-history", {id: $scope.sendId}).success(function (data) {
+        api.list("/wap/chat/message-history",{id:$scope.sendId}).success(function (data) {
             $scope.historyList = data;
 
         }).error(function () {
@@ -88,38 +99,38 @@ define(['app/module', 'chat/wechatInterface', 'chat/chat', 'app/directive/direct
         })
 
         // socket聊天
-        //requirejs(['chat/wechatInterface','chat/chat'] , function (wx , chat) {
+        requirejs(['chat/wechatInterface','chat/chat'] , function (wx , chat) {
 
-        // 获取微信配置文件
-        var config = api.wxConfig(wx, chat);
-        config.success(function (data) {
-            wx.setConfig(data.config);
-            chat.init($scope.name);
-        })
+            // 获取微信配置文件
+            var config = api.wxConfig(wx , chat);
+            config.success(function (data) {
+                wx.setConfig(data.config);
+                chat.init($scope.name);
+            })
 
 
-        // 发送消息
-        $scope.send = function () {
+            // 发送消息
+            $scope.send = function () {
 
-            if ($scope.message == '' || $scope.message == null) return;
+                if ($scope.message == '' || $scope.message == null) return;
 
-            chat.sendMessage($scope.message, $scope.sendId, 'send')
-        }
-
-        // 消息响应回调函数
-        chat.onMessageCallback = function (msg) {
-            var response = JSON.parse(msg.data);
-            switch (response.type) {
-                case 'send':
-                    console.log(response);
-                    response.id = $scope.historyList[$scope.historyList.length - 1].id + 1;
-                    $scope.historyList.push(response);
-                    break;
-
+                chat.sendMessage($scope.message , $scope.sendId ,'send')
             }
-            $scope.$apply();
-        }
-        //})
+
+            // 消息响应回调函数
+            chat.onMessageCallback = function (msg) {
+                var response = JSON.parse(msg.data);
+                switch (response.type) {
+                    case 'send':
+                        console.log(response);
+                        response.id = $scope.historyList[$scope.historyList.length-1].id + 1;
+                        $scope.historyList.push(response);
+                        break;
+
+                }
+                $scope.$apply();
+            }
+        })
 
     }]);
 })
