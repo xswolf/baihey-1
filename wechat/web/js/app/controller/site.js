b6d4f9ef2a8fed8e9afab8af7fa9dd7c6302a607
sg <825512123@qq.com>
2016-04-14 16:57:24 +0800

    调试

--- wechat/web/js/app/controller/site.js
+++ wechat/web/js/app/controller/site.js
@@ -2,7 +2,7 @@
  * Created by NSK. on 2016/4/5/0005.
  */
 define(['app/module', 'app/directive/directiveApi'
-    , 'app/service/serviceApi'
+    , 'app/service/serviceApi', 'app/filter/filterApi'
 ], function (module) {
 
     module.controller("site.index", ['app.serviceApi', '$scope', '$timeout', '$ionicPopup', '$ionicModal', '$ionicActionSheet', '$ionicLoading', function (api, $scope, $timeout, $ionicPopup, $ionicModal, $ionicActionSheet, $ionicLoading) {
