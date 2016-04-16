a713303fd0a237267ba294a9c4cd359f483a1805
499478785 <499478785@qq.com>
2016-04-16 14:06:48 +0800

    wechat

--- wechat/web/js/app.js
+++ wechat/web/js/app.js
@@ -6,8 +6,13 @@ require.config({
     baseUrl: '/wechat/web/js/',
     paths: {
         jquery: 'plugin/jquery/jquery',
-        angular: '//cdn.bootcss.com/ionic/1.2.4/js/ionic.bundle.min',
+        //angular: '//cdn.bootcss.com/ionic/1.2.4/js/ionic.bundle.min',
+        angular: 'plugin/angular/angular.min',
+        angular_animate: 'plugin/angular/angular-animate.min',
+        angular_sanitize: 'plugin/angular/angular-sanitize.min',
+        angular_ui_router: 'plugin/angular/angular-ui-router.min',
         ionic:'plugin/ionic/ionic.min',
+        ionic_angular:'plugin/ionic/ionic-angular.min',
         bootstrap:'plugin/bootstrap/bootstrap.min',
         amezeui: 'plugin/amezeui/amazeui.min',
         comm: 'comm',
@@ -23,10 +28,13 @@ require.config({
     }
 });
 
+require(['angular','angular_animate'] , function (angular) {
+    require(['ionic','ionic_angular','angular_sanitize','angular_ui_router','comm',"app/controller/listController",'plugin/angular/angular-file-upload.min'],function(comm){
+        'use strict';
 
-require(['comm','angular',"app/controller/listController"],function(comm,angular){
-    'use strict';
+        angular.bootstrap(document,['webApp']);
 
-    angular.bootstrap(document,['webApp']);
+    });
 
-});
+})
+
