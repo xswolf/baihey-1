955e67b3504375c93c53978f4321052a3d5d4266
499478785@qq.com <499478785@qq.com>
2016-04-21 15:16:20 +0800

    cache

--- wechat/web/js/app/filter/filterApi.js
+++ wechat/web/js/app/filter/filterApi.js
@@ -5,7 +5,7 @@ define(['app/module'], function (module) {
 
     // 有时间戳计算年龄
     function getAge(time) {
-        return Math.floor((ar.timeStamp() / 1000 - time) / 365 / 24 / 3600);
+        return Math.floor((time - ar.timeStamp() / 1000) / 365 / 24 / 3600);
     }
     // 由时间戳计算年龄
     module.filter('timeToAge', function () {
