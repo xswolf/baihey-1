15a533cfc7e75a5ff009175dc54c771075e1c198
sg <825512123@qq.com>
2016-04-22 18:16:20 +0800

    消息

--- wechat/controllers/MessageController.php
+++ wechat/controllers/MessageController.php
@@ -15,7 +15,8 @@ class MessageController extends BaseController
 
     public function init()
     {
-        if(!$this->isLogin()) {
+        parent::init();
+        if (!$this->isLogin()) {
             return $this->redirect('/wap/user/login');
         }
     }
@@ -27,19 +28,13 @@ class MessageController extends BaseController
      */
     public function actionIndex()
     {
-        /*if(!$this->isLogin()) {
-            return $this->redirect('/wap/user/login');
-        }*/
-        UserMessage::getInstance()->messageList($this->get);
+        $user_id = \common\util\Cookie::getInstance()->getCookie('bhy_id');
+        UserMessage::getInstance()->messageList($this->get, $user_id);
         return $this->render();
     }
 
     public function actionChat()
     {
-        if (!$this->isLogin()) {
-//            $this->redirect("/wap/user/login");
-        }
-
         $config = str_replace("\"", "'", json_encode(\Yii::$app->wechat->jsApiConfig([], false)));
         $this->assign('config', $config);
         $this->assign('id', \common\util\Cookie::getInstance()->getCookie('bhy_id'));
@@ -52,7 +47,7 @@ class MessageController extends BaseController
     public function actionMessageHistory()
     {
         $sendId = \common\util\Cookie::getInstance()->getCookie('bhy_id');
-        $list   = Message::getInstance()->getMessageHistory($sendId, \Yii::$app->request->get('id'));
+        $list = Message::getInstance()->getMessageHistory($sendId, \Yii::$app->request->get('id'));
         $this->renderAjax($list);
     }
 
@@ -70,7 +65,8 @@ class MessageController extends BaseController
      */
     public function actionGetMessageSum()
     {
-        $list = UserMessage::getInstance()->messageSum();
+        $user_id = \common\util\Cookie::getInstance()->getCookie('bhy_id');
+        $list = UserMessage::getInstance()->messageSum($user_id);
         echo $list['sumSend'];
         //$this->renderAjax(['status=>1', 'data' => $list['sumSend']]);
     }
@@ -81,7 +77,8 @@ class MessageController extends BaseController
     public function actionDel()
     {
         if (isset($this->get)) {
-            $list = UserMessage::getInstance()->messageDel($this->get);
+            $user_id = Cookie::getInstance()->getCookie('bhy_id');
+            $list = UserMessage::getInstance()->messageDel($this->get, $user_id);
         } else {
             $list = 0;
         }
@@ -91,32 +88,33 @@ class MessageController extends BaseController
     /**
      * 发送红包
      */
-    public function actionSendBribery(){
+    public function actionSendBribery()
+    {
 
         if (!$this->isLogin()) { // 未登录用户返回失败
             return $this->renderAjax(['status' => 0, 'message' => '用户未登录']);
         }
         $get = \Yii::$app->request->get();
-        $sendId     = $get['sendId'];
-        $receiveId  = $get['receiveId'];
-        $money      = (float)$get['money'];
+        $sendId = $get['sendId'];
+        $receiveId = $get['receiveId'];
+        $money = (float)$get['money'];
         $briMessage = isset($get['bri_message']) ? $get['bri_message'] : '';
 
-        if (\common\util\Cookie::getInstance()->getCookie("bhy_id") != $sendId){
+        if (\common\util\Cookie::getInstance()->getCookie("bhy_id") != $sendId) {
             // 非自己登陆的账号
             return $this->renderAjax(['status' => -1, 'message' => '非法请求']);
-        }else if ($money<=0.01 || $money>200){
+        } else if ($money <= 0.01 || $money > 200) {
             // 发送金额不符合要求
             return $this->renderAjax(['status' => -2, 'message' => '非法请求']);
         }
 
-        $userInfo = \wechat\models\User::getInstance()->getUserByName(\common\util\Cookie::getInstance()->getCookie( 'bhy_u_name' ));
-        if ($userInfo['balance'] < $money){
+        $userInfo = \wechat\models\User::getInstance()->getUserByName(\common\util\Cookie::getInstance()->getCookie('bhy_u_name'));
+        if ($userInfo['balance'] < $money) {
             // 账户余额不够
             return $this->renderAjax(['status' => -3, 'message' => '余额不够']);
         }
 
-        if ($id = UserMessage::getInstance()->sendBribery($sendId,$receiveId,$money,$briMessage)) {
+        if ($id = UserMessage::getInstance()->sendBribery($sendId, $receiveId, $money, $briMessage)) {
             $this->renderAjax(['status' => 1, 'message' => $id]);
         } else {
             $this->renderAjax(['status' => 999, 'message' => '发送失败']);
--- wechat/models/UserMessage.php
+++ wechat/models/UserMessage.php
@@ -15,30 +15,28 @@ class UserMessage extends \common\models\Base
 {
 
     protected $_user_information_table = 'user_information';
-    protected $_user_table             = 'user';
+    protected $_user_table = 'user';
 
     /**
      * 查询消息列表
      * @param array $where
      * @return array
      */
-    public function messageList($where = [])
+    public function messageList($where = [], $user_id)
     {
-
-        $user_id              = Cookie::getInstance()->getCookie('bhy_id')->value;
-        $pageSize             = 20;
-        $where['pageNum']     = isset($where['pageNum']) ? $where['pageNum'] : 1;
-        $offset               = ($where['pageNum'] - 1) * $pageSize;
-        $userTable            = $this->getDb()->tablePrefix . $this->_user_table;
+        $pageSize = 20;
+        $where['pageNum'] = isset($where['pageNum']) ? $where['pageNum'] : 1;
+        $offset = ($where['pageNum'] - 1) * $pageSize;
+        $userTable = $this->getDb()->tablePrefix . $this->_user_table;
         $userInformationTable = $this->getDb()->tablePrefix . $this->_user_information_table;
-        $sql                  = "SELECT u.username,u.sex,u.phone,i.info,i.identity_pic,c.* FROM $userTable u INNER JOIN $userInformationTable i ON u.id=i.user_id
+        $sql = "SELECT u.username,u.sex,u.phone,i.info,i.identity_pic,c.* FROM $userTable u INNER JOIN $userInformationTable i ON u.id=i.user_id
 INNER JOIN
 (
 SELECT m.id,m.send_user_id,m.receive_user_id,message,m.time,STATUS,tmp.sumSend, m.send_user_id other FROM bhy_user_message  m INNER JOIN (
 SELECT send_user_id,COUNT(send_user_id) sumSend,MAX(TIME) TIME FROM bhy_user_message WHERE receive_user_id = $user_id AND STATUS=2 GROUP BY send_user_id
 )tmp ON m.send_user_id = tmp.send_user_id AND m.time = tmp.time
 ) c ON u.id=c.other GROUP BY c.other ORDER BY time DESC limit $offset,$pageSize";
-        $result               = $this->getDb()->createCommand($sql)->queryAll();
+        $result = $this->getDb()->createCommand($sql)->queryAll();
 
         return $result;
     }
@@ -47,11 +45,10 @@ SELECT send_user_id,COUNT(send_user_id) sumSend,MAX(TIME) TIME FROM bhy_user_mes
      * 获取最新消息总条数
      * @return array|bool
      */
-    public function messageSum()
+    public function messageSum($user_id)
     {
-        $user_id   = Cookie::getInstance()->getCookie('bhy_id')->value;
         $condition = ['receive_user_id' => $user_id, 'status' => 2];
-        $result    = (new Query())
+        $result = (new Query())
             ->select(['count(id) sumSend'])
             ->where($condition)
             ->from(static::tableName())
@@ -65,26 +62,24 @@ SELECT send_user_id,COUNT(send_user_id) sumSend,MAX(TIME) TIME FROM bhy_user_mes
      * @param array $where
      * @return int
      */
-    public function messageDel($where = [])
+    public function messageDel($where = [], $user_id)
     {
-
-        $user_id = Cookie::getInstance()->getCookie('bhy_id');
-        $row     = $this->updateAll(['status' => 1], ['receive_user_id' => $user_id, 'send_user_id' => $where['msgId']]);
+        $row = $this->updateAll(['status' => 1], ['receive_user_id' => $user_id, 'send_user_id' => $where['msgId']]);
         //$row += $this->updateAll(['status' => 2], ['send_user_id' => $user_id, 'receive_user_id' => $where['msgId']]);
 
         return $row;
     }
 
-    public function sendBribery($sendId, $receiveId, $money,$bri_message)
+    public function sendBribery($sendId, $receiveId, $money, $bri_message)
     {
         $model = Base::getInstance('user_bribery');
-        $model->send_user_id    = $sendId;
+        $model->send_user_id = $sendId;
         $model->receive_user_id = $receiveId;
-        $model->money           = $money;
-        $model->time            = time();
-        $model->status          = 1;
-        $model->bri_message     = $bri_message;
-        if($model->insert(true)){
+        $model->money = $money;
+        $model->time = time();
+        $model->status = 1;
+        $model->bri_message = $bri_message;
+        if ($model->insert(true)) {
             return \Yii::$app->db->lastInsertID;
         }
         return false;
