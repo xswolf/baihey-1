1a9474bf475c58602e829b8aefc10fbe4a0184e3
499478785@qq.com <499478785@qq.com>
2016-05-12 10:11:01 +0800

    ts

--- backend/controllers/ConfigController.php
+++ backend/controllers/ConfigController.php
@@ -9,11 +9,16 @@
 namespace backend\controllers;
 
 
+use yii\db\Query;
+
 class ConfigController extends BaseController
 {
 
     public function actionList(){
 
+        $list = (new Query())->from(\Yii::$app->db->tablePrefix.'config')->select("*")->all();
+
+        $this->assign('list' , $list);
 
         return $this->render();
     }
