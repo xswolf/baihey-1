e628028fe4a0e1e875a9a32826b67186e2bbd414
sg <825512123@qq.com>
2016-05-17 18:19:03 +0800

    其他人资料页

--- wechat/web/js/app/filter/filterApi.js
+++ wechat/web/js/app/filter/filterApi.js
@@ -183,8 +183,8 @@ define(['app/module'], function (module) {
     module.filter('address', function() {
         return function (input, arr1, arr2) {
             if(typeof(input) != 'undefined' && input != '') {
-                arr2 = arr2 ? getAddress(area, arr2) : '';
-                arr1 = arr1 ? getAddress(citys, arr1) : '';
+                arr2 = arr2 != '0' ? getAddress(area, arr2) : '';
+                arr1 = arr1 != '0' ? getAddress(citys, arr1) : '';
                 input = getAddress(provines, input);
                 if (arr2 != '') {
                     return input + '-' + arr1 + '-' + arr2;
