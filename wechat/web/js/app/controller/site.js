31a3af826e38a57b44df17a9130b5ba43e5382d1
Administrator <570434734@qq.com>
2016-05-22 16:21:04 +0800

    s

--- wechat/web/js/app/controller/site.js
+++ wechat/web/js/app/controller/site.js
@@ -6,56 +6,65 @@ define(['app/module', 'app/directive/directiveApi'
 ], function (module) {
 
     module.controller("site.index", ['app.serviceApi', '$scope', '$timeout', '$ionicPopup', '$ionicModal', '$ionicActionSheet', '$ionicLoading', function (api, $scope, $timeout, $ionicPopup, $ionicModal, $ionicActionSheet, $ionicLoading) {
-        $scope.searchForm = {
-            data: {}
-        }
 
-        // 默认查询条件处理（性别处理）
-        if (ar.getCookie('bhy_u_sex') && (ar.getCookie('bhy_u_sex') == 0)) {
-            $scope.searchForm.data.sex = 1;
-        } else {
-            $scope.searchForm.data.sex = 0;
-        }
-        // 获取用户地理位置。 如果为获取到，则默认重庆
-        if (ar.getCookie('bhy_u_city') && ar.getCookie('bhy_u_cityId')) {
-            $scope.cityName = eval(ar.getCookie('bhy_u_city'));
-            $scope.cityId = ar.getCookie('bhy_u_cityId');
-            $scope.searchForm.data.city = ar.getCookie('bhy_u_cityId');
-        } else {
-            $scope.cityName = '重庆';
-            $scope.cityId = 2;
-            $scope.searchForm.data.city = 2
-        }
-        // 默认查询条件,年龄范围，页码，页数
-        $scope.searchForm.data.age = '18-28';
-        $scope.searchForm.data.pageNum = 1;
-        $scope.searchForm.data.pageSize = 6;
+        // 搜索条件
+        $scope.searchForm = [];
 
-        // 地区选择查询广播
-        $scope.$on('cityName', function (event, data) {
-            $scope.cityName = data.name;
-            $scope.cityId = data.id;
-            $scope.searchForm.data.city = data.id;
-            $scope.searchForm.data.cityName = data.name;
-            $scope.searchForm.data.pageNum = 1;
-            $scope.pageLast = true;
-            api.list("/wap/site/user-list", $scope.searchForm.data).success(function (res) {
+        // 用户列表
+        $scope.userList = [];
+
+        // 默认还有更多
+        $scope.pageLast = true;
+
+        // 读取用户数据
+        function userListPromise(searchForm){
+            var data = [];
+            api.list("/wap/site/user-list", searchForm).success(function (res) {
+                data = res.data;
+                if(res.data.length == 0){
+                    $scope.pageLast = false;
+                }
                 $scope.userList = res.data;
                 for (var i in $scope.userList) {
                     $scope.userList[i].info = JSON.parse($scope.userList[i].info);
-                    $scope.userList[i].auth = JSON.parse($scope.userList[i].auth);
+                    $scope.userList[i].identity_pic = JSON.parse($scope.userList[i].identity_pic);
                 }
             })
-        });
+            return data;
+        }
 
-        // 获取默认list
-        api.list("/wap/site/user-list", $scope.searchForm.data).success(function (res) {
-            $scope.userList = res.data;
-            for (var i in $scope.userList) {
-                $scope.userList[i].info = JSON.parse($scope.userList[i].info);
-                $scope.userList[i].auth = JSON.parse($scope.userList[i].auth);
-            }
-        })
+        // 根据登录状态，登录用户性别默认查询条件：性别
+        if (ar.getCookie('bhy_u_sex') && (ar.getCookie('bhy_u_sex') == 0)) {
+            $scope.searchForm.sex = 1;
+        } else {
+            $scope.searchForm.sex = 0;
+        }
+
+        // 获取用户地理位置。 如果未获取到，则默认重庆
+        if (ar.getCookie('bhy_u_city') && ar.getCookie('bhy_u_cityId')) {
+            $scope.cityName = eval(ar.getCookie('bhy_u_city'));
+            $scope.cityId = ar.getCookie('bhy_u_cityId');
+            $scope.searchForm.city = ar.getCookie('bhy_u_cityId');
+        } else {
+            $scope.cityName = '重庆';
+            $scope.cityId = 2;
+            $scope.searchForm.city = 2
+        }
+
+        // 默认查询条件：年龄范围，页码，每页数量
+        $scope.searchForm.age = '18-28';
+        $scope.searchForm.pageNum = 0;
+        $scope.searchForm.pageSize = 6;
+
+        // 监听地区变化
+        $scope.$on('cityName', function (event, data) {
+            $scope.cityName = data.name;
+            $scope.cityId = data.id;
+            $scope.searchForm.city = data.id;
+            $scope.searchForm.cityName = data.name;
+            $scope.searchForm.pageNum = 0;
+
+        });
 
 
         // 选择城市模版
@@ -71,23 +80,14 @@ define(['app/module', 'app/directive/directiveApi'
             scope: $scope,
             animation: 'slide-in-up'
         }).then(function (modal) {
-            $scope.searchModal = modal;
             $scope.moreSearchModal = modal;
         });
 
-        $scope.moreSearchModalHide = function () {
+        // 高级搜索-点击确定
+        $scope.moreSearchOk = function () {
             $scope.moreSearchModal.hide();
-            //console.log($scope.searchForm.data);
-            $scope.searchForm.data.pageNum = 1;
-            $scope.pageLast = true;
-            $scope.user = api.list("/wap/site/user-list", $scope.searchForm.data);
-            $scope.user.success(function (res) {
-                $scope.userList = res.data;
-                for (var i in $scope.userList) {
-                    $scope.userList[i].info = JSON.parse($scope.userList[i].info);
-                    $scope.userList[i].auth = JSON.parse($scope.userList[i].auth);
-                }
-            })
+            $scope.searchForm.pageNum = 0;
+            userListPromise($scope.searchForm);
         }
 
         $scope.buttonsItemIndex = '';
@@ -113,38 +113,30 @@ define(['app/module', 'app/directive/directiveApi'
                 buttons: $scope.buttonsItem,
                 titleText: '搜索',
                 cancelText: '取消',
-                cancel: function () {
-
-                },
                 buttonClicked: function (index) {
-                    var user = null;
+                    $scope.searchForm.pageNum = 0;
                     $scope.buttonsItemIndex = index;
-
                     if (index == 0) {   // 全部
-                        $scope.searchForm.data.sex = 'all';
-                        $scope.searchForm.data.pageNum = 1;
-                        $scope.pageLast = true;
-                        user = api.list("/wap/site/user-list", $scope.searchForm.data);
+                        $scope.searchForm.sex = 'all';
+                        userListPromise($scope.searchForm);
+                        hideSheet();
                     }
 
                     if (index == 1) {   //只看男
-                        $scope.searchForm.data.sex = 1;
-                        $scope.searchForm.data.pageNum = 1;
-                        $scope.pageLast = true;
-                        user = api.list("/wap/site/user-list", $scope.searchForm.data);
+                        $scope.searchForm.sex = 1;
+                        userListPromise($scope.searchForm);
+                        hideSheet();
                     }
 
                     if (index == 2) {   //只看女
-                        $scope.searchForm.data.sex = 0;
-                        $scope.searchForm.data.pageNum = 1;
-                        $scope.pageLast = true;
-                        user = api.list("/wap/site/user-list", $scope.searchForm.data);
+                        $scope.searchForm.sex = 0;
+                        userListPromise($scope.searchForm);
+                        hideSheet();
                     }
 
                     if (index == 3) {   //高级搜索
-                        $scope.$broadcast('someEvent', $scope.searchForm.data);
-                        $scope.searchModal.show();
-
+                        $scope.moreSearchModal.show();
+                        hideSheet();
                     }
 
                     if (index == 4) {   //ID搜索
@@ -156,60 +148,29 @@ define(['app/module', 'app/directive/directiveApi'
                             okType: 'button-energized',
                             inputPlaceholder: '请输入对方ID号'
                         }).then(function (res) {
-                            api.list("/wap/site/user-list", {'id': res}).success(function (res) {
-                                $scope.userList = res.data;
-                                $scope.userList[0].info = JSON.parse($scope.userList[0].info);
-                                $scope.userList[i].auth = JSON.parse($scope.userList[i].auth);
-                            })
+                            $scope.searchForm.id = res;
+                            userListPromise($scope.searchForm);
+                            hideSheet();
                         });
                     }
-                    if (user != undefined) {
-                        user.success(function (res) {
-                            $scope.userList = res.data;
-                            for (var i in $scope.userList) {
-                                $scope.userList[i].info = JSON.parse($scope.userList[i].info);
-                                $scope.userList[i].auth = JSON.parse($scope.userList[i].auth);
-                            }
-                        })
-                    }
-
-
-                    return true;
                 }
             });
+        }
 
+        // 加载更多
+        $scope.loadMore = function () {
+           var data = userListPromise($scope.searchForm);
+           $scope.userList = $scope.userList.concat(data);
+           $scope.$broadcast('scroll.infiniteScrollComplete');
+           $scope.searchForm.pageNum++;
         }
 
         // 是否还有更多
-        $scope.pageLast = true;
-
-        // 点击加载更多
-        $scope.loadMore = function () {
-            $scope.searchForm.data.pageNum++;
-
-            api.list('/wap/site/user-list', $scope.searchForm.data).success(function (res) {
-
-                if (res.data.length < $scope.searchForm.data.pageSize) {
-                    $scope.pageLast = false;
-                }
-
-                for (var i in res.data) {
-                    res.data[i].info = JSON.parse(res.data[i].info);
-                    res.data[i].auth = JSON.parse(res.data[i].auth);
-                }
-                $scope.userList = $scope.userList.concat(res.data);
-
-
-            });
-
+        $scope.moreDataCanBeLoaded = function () {
+            return $scope.pageLast;
         }
 
-    }]);
-
-    //选择城市
-    module.controller('site.childCityController', ['app.serviceApi', '$scope', function (api, $scope) {
-
-
+        // 选择城市
         $scope.bodyHeight = document.body.scrollHeight;
         if ($scope.bodyHeight == 0) $scope.bodyHeight = window.screen.height;
         $scope.scrollStyle = {
@@ -230,34 +191,25 @@ define(['app/module', 'app/directive/directiveApi'
 
         $scope.selected_city = function (city_id) {
             $scope.cityId = city_id;
-            //$scope.searchForm.data.city = city_id;
         }
 
         // 保存已选择城市
         $scope.citySave = function () {
-
             for (var i = 0; i < $scope.citys.length; i++) {
                 if ($scope.citys[i].id == $scope.cityId) {
                     $scope.modalCityName = $scope.citys[i].name;
-                    $scope.$emit('cityName', $scope.citys[i]);
+                    $scope.cityName = $scope.citys[i].name;
                     continue;
                 }
             }
-
             $scope.cityModal.hide();
         }
 
-        $scope.provinces = provines;
-        $scope.citys = citys;
+        $scope.provinceList = provines;
+        $scope.cityList = citys;
 
-    }]);
-
-    // 高级搜索
-    module.controller('site.childSearchController', ['app.serviceApi', '$scope', function (api, $scope) {
-        $scope.$on('someEvent', function (event, mass) {
-            $scope.searchForm.data = mass;
-        });
-        $scope.searchForm = {};
+        /* 高级搜索 */
+        //$scope.searchForm = {};
         $scope.salary = config_infoData.salary;
         $scope.house = config_infoData.house;
         $scope.marriage = config_infoData.marriage;
@@ -269,13 +221,12 @@ define(['app/module', 'app/directive/directiveApi'
         $scope.constellation = config_infoData.constellation;
         $scope.nation = config_infoData.nation;
 
-        $scope.searchForm.sex = 'all';
+        //$scope.searchForm.sex = 'all';
         // 职业赋值
         $scope.occupations = occupation;
 
-        $scope.searchForm.clickSex = function (value) {
+        $scope.sexChange = function (value) {
             $scope.searchForm.sex = value;
-            $scope.searchForm.data.sex = value;
         }
 
         $scope.moreText = '展开';
@@ -289,16 +240,10 @@ define(['app/module', 'app/directive/directiveApi'
             }
         }
 
+        // 默认搜索条件
+
 
     }]);
-
-    module.controller('footer', ['app.serviceApi', '$scope', '$location', function (api, $scope, $location) {
-        $scope.menu = $location.url();
-        $scope.switchMenu = function (menu) {
-            $scope.menu = menu;
-        }
-    }]);
-
 
 
 })
