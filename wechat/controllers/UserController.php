82903e06642af0a448f3912dd6a7a29783570e21
Administrator <570434734@qq.com>
2016-06-03 15:09:44 +0800

    s

--- wechat/controllers/UserController.php
+++ wechat/controllers/UserController.php
@@ -103,7 +103,7 @@ class UserController extends BaseController
     {
         $user = $this->weChatMember();
 
-        $this->actionWxLogin();
+        var_dump($user);
 
         $wxJSSDK = new WeChat();
         $signPackage = $wxJSSDK->getSignPackage();
