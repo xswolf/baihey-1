e129929169ab001770d3cb47d9c21109150f0beb
499478785@qq.com <499478785@qq.com>
2016-06-28 09:38:55 +0800

    ts

--- nodejs/model/Message.js
+++ nodejs/model/Message.js
@@ -12,6 +12,7 @@ function Message() {
         conn.query(sql , [msg.send_user_id,msg.receive_user_id,msg.message,this.getMessageType(msg.type),time,msg.status] , function (err ,res) {
             callback(err, res);
         })
+        conn.end();
     }
 
     // 获取消息类型
