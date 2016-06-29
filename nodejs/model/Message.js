775c6a53022fd57b054f23b530617fca39d19d91
499478785@qq.com <499478785@qq.com>
2016-06-29 10:07:03 +0800

    ts

--- nodejs/model/Message.js
+++ nodejs/model/Message.js
@@ -5,7 +5,7 @@ function Message() {
 
     // 发送信息记录数据库
     this.add = function (msg, callback) {
-        var sql = 'insert into bhy_user_message (send_user_id , receive_user_id,message,message_type,create_time,status) values(?,?,?,?,?,?)';
+        var sql = 'inser into bhy_user_message (send_user_id , receive_user_id,message,message_type,create_time,status) values(?,?,?,?,?,?)';
         var time = Date.parse(new Date()) / 1000;
         //var sql = 'insert into bhy_message set send_user_id = ? ,receive_user_id=?, message=?,message_type=?,create_time=?,status=?';
         var conn = mysql.connection();
