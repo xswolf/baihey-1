2fd454a5f6f58d61cce557fd10edcbaf89f6dceb
sg <825512123@qq.com>
2016-06-30 17:48:15 +0800

    个人动态

--- wechat/controllers/MemberController.php
+++ wechat/controllers/MemberController.php
@@ -158,7 +158,8 @@ class MemberController extends BaseController
         isset($this->get['page']) ? $page = $this->get['page'] : $page = 0;
         isset($this->get['user_id']) ? $userId = $this->get['user_id'] : $userId = -1;
 
-        $list = User::getInstance()->getDynamicList($userId, $page);
+        //$list = User::getInstance()->getDynamicList($userId, $page);
+        $list = UserDynamic::getInstance()->getDynamicList($userId, $page);
         $this->renderAjax(['status' => 1, 'data' => $list]);
     }
 
@@ -207,26 +208,6 @@ class MemberController extends BaseController
     /**
      * 发布个人动态
      */
-    public function actionAddDynamic()
-    {
-
-        $user_id = Cookie::getInstance()->getCookie('bhy_id')->value;
-
-        $data['user_id'] = $user_id;
-        $data['auth'] = $this->get['auth'];
-        $data['address'] = isset($this->get['address']) ? $this->get['address'] : '';
-        $data['name'] = $this->get['name'];
-        $data['pic'] = isset($this->get['pic']) ? $this->get['pic'] : '';
-        $data['content'] = isset($this->get['content']) ? $this->get['content'] : '';
-        $flag = User::getInstance()->addDynamic($data);
-        if ($flag > 0) {
-            $data = User::getInstance()->getDynamicById(\Yii::$app->db->lastInsertID);
-            $this->renderAjax(['status' => 1, 'data' => $data[0]]);
-        } else {
-            $this->renderAjax(['status' => -1, 'data' => '发布失败']);
-        }
-    }
-
     public function actionAddUserDynamic()
     {
         $user_id = Cookie::getInstance()->getCookie('bhy_id')->value;
@@ -238,6 +219,7 @@ class MemberController extends BaseController
         }
 
     }
+
     /**
      * 获取单条动态内容
      */
