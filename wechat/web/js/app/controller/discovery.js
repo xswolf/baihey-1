c17a917a5b374644aead8ea86d5d49ce8fd8df5b
Administrator <570434734@qq.com>
2016-06-30 21:36:27 +0800

    s

--- wechat/web/js/app/controller/discovery.js
+++ wechat/web/js/app/controller/discovery.js
@@ -219,6 +219,7 @@ define(['app/module', 'app/directive/directiveApi'
             $scope.formData = {};
             $scope.isMore = true;
             $scope.pageSize = 5;
+            $scope.commentList = [];
             $scope.jump = function (id) {
                 if(id == $scope.userInfo.id) {
                     $location.url('/member/information');
