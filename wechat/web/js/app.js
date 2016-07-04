4f903d04a3c186a9010b76a5d7c48e98d238d695
499478785@qq.com <499478785@qq.com>
2016-07-04 15:55:59 +0800

    ts

--- wechat/web/js/app.js
+++ wechat/web/js/app.js
@@ -2,13 +2,12 @@
  * Created by Administrator on 2016/3/29.
  */
 require.config({
-    urlArgs: "bust=v3"+Math.random(), // 清除缓存
+    //urlArgs: "bust=v3"+Math.random(), // 清除缓存
     baseUrl: '/wechat/web/js/',
     paths: {
         jquery: 'plugin/jquery/jquery',
         //jquery_1_8_3: 'plugin/jquery/jquery_1.8.3.min',
-        ionic: '//cdn.bootcss.com/ionic/1.2.4/js/ionic.bundle.min',
-        //ionic: 'plugin/ionic/ionic.bundle',
+        ionic: ['//cdn.bootcss.com/ionic/1.2.4/js/ionic.bundle.min' , 'plugin/ionic/ionic.bundle'],
         angular: 'plugin/angular/angular.min',
         angular_animate: 'plugin/angular/angular-animate.min',
         angular_sanitize: 'plugin/angular/angular-sanitize.min',
@@ -34,7 +33,10 @@ require.config({
             exports:"angular"
         },
         ionic:{
-            exports:"ionic"
+            exports:"ionic",
+            deps:[
+
+            ]
         }
     }
 });
