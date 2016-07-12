74138b3f289c12e572411fc9072ffd2062a50378
Administrator <570434734@qq.com>
2016-07-12 11:47:19 +0800

    s

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -288,8 +288,8 @@
                         <span class="arrow"></span>
                     </a>
                     <ul class="sub">
-                        <li class="has-sub-sub"><a data-menu="7" class="has-sub-sub"  href="/admin/member/index"><span class="sub-menu-text">会员查找</span></a></li>
-                        <li class="has-sub-sub"><a data-menu="8" class="has-sub-sub"  href="/admin/member/photo?is_check=2&type=1"><span class="sub-menu-text">照&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;片</span></a></li>
+                        <li class="has-sub-sub"><a data-menu="7" class="has-sub-sub"  href="/admin/member/index"><span class="sub-menu-text">会员列表</span></a></li>
+                        <li class="has-sub-sub"><a data-menu="8" class="has-sub-sub"  href="/admin/member/photo?is_check=2&type=1"><span class="sub-menu-text">照片审核</span></a></li>
                         <li class="has-sub-sub"><a data-menu="9" class="has-sub-sub"  href="/admin/member/order"><span class="sub-menu-text">订单列表</span></a></li>
                         <li class="has-sub-sub"><a data-menu="10" class="has-sub-sub"  href="/admin/cash/index?status=2"><span class="sub-menu-text">提现管理</span></a></li>
                         <li class="has-sub-sub"><a data-menu="11" class="has-sub-sub"  href="/admin/chat/index"><span class="sub-menu-text">聊天管理</span></a></li>
