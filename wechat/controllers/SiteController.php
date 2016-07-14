c6b5a3ad0e01e76173c844490f2e74b3fdb30523
sg <825512123@qq.com>
2016-07-14 17:51:34 +0800

    登录限制测试

--- wechat/controllers/SiteController.php
+++ wechat/controllers/SiteController.php
@@ -47,7 +47,7 @@ class SiteController extends BaseController
         if($user['status'] > 2) {
             Cookie::getInstance()->delLoginCookie();
             if($user['wx_id']) {
-                setcookie('wx_login', false, time() + 5, '/wap');
+                setcookie('wx_login', false, time() + 2, '/wap');
             }
         }
         return $this->render();
