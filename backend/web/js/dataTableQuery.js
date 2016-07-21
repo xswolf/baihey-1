172d63150e3022de4314861f2dae22657cbd4459
Administrator <570434734@qq.com>
2016-07-21 19:09:49 +0800

    s

--- backend/web/js/dataTableQuery.js
+++ backend/web/js/dataTableQuery.js
@@ -132,7 +132,7 @@ $(function(){
                     $(nTd).html(html)
                 }},
                 {"data" : "id","fnCreatedCell":function (nTd, sData, oData, iRow, iCol) {
-                    var html = '<a class="btn btn-primary btn-sm" href="/admin/member/info?id=' +oData.id+ '">管理</a> <a class="btn btn-info btn-sm" href="javascript:;">回访</a> <a class="btn btn-info btn-sm" href="javascript:;">配对</a>';
+                    var html = '<a class="btn btn-primary btn-sm" href="/admin/member/info?id=' +oData.id+ '">管理</a> <a class="btn btn-info btn-sm" data-uid="'+oData.id+'" id="returningBtn" href="javascript:;">回访</a> <a class="btn btn-info btn-sm" href="javascript:;">配对</a>';
                     $(nTd).html(html);
                 }}
             ]
