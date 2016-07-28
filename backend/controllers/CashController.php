82d77497f40f30d465586d7dc086276c304a0508
sg <825512123@qq.com>
2016-07-28 13:21:29 +0800

    提现列表

--- backend/controllers/CashController.php
+++ backend/controllers/CashController.php
@@ -16,9 +16,26 @@ class CashController extends BaseController
 
     // 提现列表
     public function actionIndex(){
-
-        $status = \Yii::$app->request->get('status');
-        $list = Cash::getInstance()->lists($status);
+        $andWhere = [];
+        if($this->get) {
+            if (isset($this->get['id_phone_name']) && $this->get['id_phone_name'] != '') { // 电话、ID、姓名
+                $id_phone_name = $this->get['id_phone_name'];
+                if (is_numeric($id_phone_name)) {
+                    if (strlen($id_phone_name . '') == 11) {
+                        $andWhere[] = ["=", "u.phone", $id_phone_name];
+                    } else {
+                        $andWhere[] = ["=", "cash.user_id", $id_phone_name];
+                    }
+                } else {
+                    $andWhere[] = ["like", "card.user_name", $id_phone_name];
+                }
+            }
+            if ($this->get['status'] != ''){
+                $andWhere[] = ['=' , 'cash.status' , $this->get['status']];
+            }
+        }
+        //$status = \Yii::$app->request->get('status');
+        $list = Cash::getInstance()->lists($andWhere);
         $this->assign('list' , $list);
         return $this->render();
     }
--- common/models/Cash.php
+++ common/models/Cash.php
@@ -9,13 +9,19 @@ class Cash extends Base
 {
 
 
-    public function lists( $status = 2){
+    public function lists($where = []){
 
         $handle = (new Query())->from($this->tablePrefix.'cash cash')
             ->innerJoin($this->tablePrefix.'cash_card card' , 'cash.cash_card_id=card.id')
-            ->select("cash.*, card.user_name,card.card_no,card.name")
-            ->where(['status' => $status]);
-
+            ->innerJoin($this->tablePrefix . 'user u', 'cash.user_id = u.id')
+            ->leftJoin($this->tablePrefix . 'auth_user au', 'cash.admin_id = au.id')
+            ->select("cash.*, card.user_name,card.card_no,card.name, au.name as admin_name");
+            //->where(['status' => $status]);
+        if (count($where) > 0) {
+            foreach ($where as $v) {
+                $handle->andWhere($v);
+            }
+        }
         return $handle->all();
     }
 
@@ -25,9 +31,12 @@ class Cash extends Base
      * @return bool
      */
     public function confirm($id){
-
+        $admin = Yii::$app->session->get(USER_SESSION);
+        $data['admin_id'] = $admin['id'];
+        $data['end_time'] = time();
+        $data['status'] = 1;
         return \Yii::$app->db->createCommand()
-            ->update($this->tablePrefix.'cash' , ['status' => 1] , ['id' => $id])
+            ->update($this->tablePrefix.'cash' , $data , ['id' => $id])
             ->execute();
     }
 
