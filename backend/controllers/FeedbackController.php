4793be4e69dcaf26c367b926ba0609fe01078e49
sg <825512123@qq.com>
2016-07-31 16:51:10 +0800

    举报管理

--- backend/controllers/FeedbackController.php
+++ backend/controllers/FeedbackController.php
@@ -45,11 +45,32 @@ class FeedbackController extends BaseController
     public function actionAuth()
     {
         $id = \Yii::$app->request->get('id');
-        if($this->post['status'] != 3) {
+        $data = $this->post;
+        if($data['status'] != 3) {
             var_dump($this->post);
+            if($data['type'] == 1) {
+                $content = $data['content'];
+            } elseif($data['type'] == 2) {
+                $content = $data['content'];
+            } elseif($data['type'] == 3) {
+                $content = $data['content'];
+            }
+            // 此处处理发送给被举报人
+
+
+            // 是否发送给举报人
+            if(isset($data['ret']) && $data['ret'] == 'on') {
+                echo '感谢您对本网站文明建设的支持，经查证情况属实，我们已对该账号做出相应处理';
+            }
+        } else {
+            // 是否发送给举报人
+            if (isset($data['ret']) && $data['ret'] == 'on') {
+                echo '感谢您对本网站文明建设的支持，但因证据不足，暂时不予处理';
+            }
         }
+        var_dump($this->post);
         exit;
-        $id = Feedback::getInstance()->auth($id, $this->post['status']);
+        $id = Feedback::getInstance()->auth($id, $data['status']);
         if ($id>0){
             $this->renderAjax(['status'=>1 , 'message'=>'成功']);
         }else{
