01178f10eb2ed74790b250f02ce7dffab0dabef7
499478785@qq.com <499478785@qq.com>
2016-08-01 17:35:12 +0800

    ts

--- console/controllers/ConsoleController.php
+++ console/controllers/ConsoleController.php
@@ -29,7 +29,9 @@ SET age = IF ({$m} - DATE_FORMAT(DATE_ADD(FROM_UNIXTIME(0), INTERVAL json_extrac
 
                 {$y} - DATE_FORMAT(DATE_ADD(FROM_UNIXTIME(0), INTERVAL json_extract(info,'$.age')+0 SECOND) , '%Y') -1 ,
 
-                {$y} - DATE_FORMAT(DATE_ADD(FROM_UNIXTIME(0), INTERVAL json_extract(info,'$.age')+0 SECOND) , '%Y')) ";
+                {$y} - DATE_FORMAT(DATE_ADD(FROM_UNIXTIME(0), INTERVAL json_extract(info,'$.age')+0 SECOND) , '%Y'))
+
+                 WHERE json_extract(info,'$.age')  >0 AND json_extract(info,'$.age') != '' ";
 
         if (\Yii::$app->db->createCommand($sql)->execute()) {
             echo 'age is update';
