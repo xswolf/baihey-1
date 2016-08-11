f566da07a88ff0d1903ff17dabcf6c5ddefd7e84
499478785@qq.com <499478785@qq.com>
2016-08-11 16:51:14 +0800

    ts

--- wechat/web/js/app/router.js
+++ wechat/web/js/app/router.js
@@ -59,7 +59,8 @@ define(["app/module", 'app/service/serviceApi'],
 
             // 查询是否有新的用户关注自己   TODO
             $rootScope.newFollow = false;
-            if(userId != '') {
+            alert(userId)
+            if(userId != null) {
                 api.get('/wap/follow/is-new-follow', {user_id: userId}).success(function (res) {
                     $rootScope.newFollow = res.status;
                 })
