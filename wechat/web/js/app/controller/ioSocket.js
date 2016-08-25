fc6313b18d77e61b142595f3b0736d09dfeea6e7
499478785@qq.com <499478785@qq.com>
2016-08-25 14:31:33 +0800

    ts

--- wechat/web/js/app/controller/ioSocket.js
+++ wechat/web/js/app/controller/ioSocket.js
@@ -566,9 +566,9 @@ define(['app/module', 'app/directive/directiveApi'
                     response.message = response.message.replace(/&quot;/g, "\"");
                     if ($scope.sendId == response.send_user_id) {  // 响应自己发送的消息
                         for (var i in $scope.historyList) {
-                            if (response.status == 1 && $scope.historyList[i].status != 4) { // 如果对方在线，所有消息均设置已读
-                                $scope.historyList[i].status = 1;
-                            }
+                            //if (response.status == 1 && $scope.historyList[i].status != 4) { // 如果对方在线，所有消息均设置已读
+                            //    $scope.historyList[i].status = 1;
+                            //}
                             if (response.time == $scope.historyList[i].time &&
                                 (response.message == $scope.historyList[i].message ||
                                 response.type == 'pic')) {
