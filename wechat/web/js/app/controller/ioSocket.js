4ab5a78cad3ec7c852f28695b2dd3bcd7242c87c
499478785@qq.com <499478785@qq.com>
2016-08-25 12:33:13 +0800

    ts

--- wechat/web/js/app/controller/ioSocket.js
+++ wechat/web/js/app/controller/ioSocket.js
@@ -538,8 +538,6 @@ define(['app/module', 'app/directive/directiveApi'
                 var setMessage = function (response) {
                     if (response.type == 'madd' || response.type == 'remove' || response.type == 'add') return;
                     response.message = response.message.replace(/&quot;/g, "\"");
-                    console.log(response);
-                    console.log($scope.historyList);
                     if ($scope.sendId == response.send_user_id) {  // 响应自己发送的消息
                         for (var i in $scope.historyList) {
                             if (response.status == 1) { // 如果对方在线，所有消息均设置已读
