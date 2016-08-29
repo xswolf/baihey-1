c11ab2cf9028026a75f3758aac8e2555581521dd
499478785@qq.com <499478785@qq.com>
2016-08-29 17:42:45 +0800

    ts

--- wechat/controllers/ServerController.php
+++ wechat/controllers/ServerController.php
@@ -50,15 +50,14 @@ class ServerController extends BaseController {
 
             $userInfo = \Yii::$app->wechat->getMemberInfo($fromUsername);
             if(is_array($userInfo) && count($userInfo) > 0){
-//                file_put_contents('./log.txt' , $fromUsername."\n" ,FILE_APPEND);
+                file_put_contents('./log.txt' , $fromUsername."\n" ,FILE_APPEND);
             }else{
                 file_put_contents('./log.txt' ,"chuxiancuowu\n" ,FILE_APPEND);
             }
-            echo $resultStr;
             file_put_contents('./log.txt' , $resultStr."\n" ,FILE_APPEND);
-            //\Yii::$app->wechat->sendMaterial($fromUsername , "TtSb9HO50njLDfRLrBEM_NKXrzVpIgfX9DYtwftdrGQ");
-
+            echo $resultStr;
             exit;
+
         } else {
             echo "";
             exit;
