913892f6df9b80c1f640a7484d6897d5a1585226
499478785@qq.com <499478785@qq.com>
2016-08-29 17:31:20 +0800

    ts

--- common/wechat/WeChat.php
+++ common/wechat/WeChat.php
@@ -56,20 +56,20 @@ class WeChat extends \callmez\wechat\sdk\Wechat {
 
     public function responseNews( $fromUserName,$toUserName ) {
         $newTpl = "<xml>
-                    <ToUserName><![CDATA[%s]]></ToUserName>
-                    <FromUserName><![CDATA[%s]]></FromUserName>
-                    <CreateTime>%s</CreateTime>
-                    <MsgType><![CDATA[news]]></MsgType>
-                    <ArticleCount>1</ArticleCount>
-                    <Articles>
-                    <item>
-                    <Title><![CDATA[%s]]></Title>
-                    <Description><![CDATA[%s]]></Description>
-                    <PicUrl><![CDATA[%s]]></PicUrl>
-                    <Url><![CDATA[%s]]></Url>
-                    </item>
-                    </Articles>
-                   </xml>";
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[news]]></MsgType>
+<ArticleCount>1</ArticleCount>
+<Articles>
+<item>
+<Title><![CDATA[%s]]></Title>
+<Description><![CDATA[%s]]></Description>
+<PicUrl><![CDATA[%s]]></PicUrl>
+<Url><![CDATA[%s]]></Url>
+</item>
+</Articles>
+</xml>";
         $resultStr = sprintf($newTpl,$toUserName,$fromUserName,time(),'wel','join','','');
         return $resultStr;
     }
