de5c49e93c3d6bf8a7b4c78162ab3af07dc46fbc
Administrator <570434734@qq.com>
2016-08-30 14:29:20 +0800

    nsk

--- wechat/controllers/ServerController.php
+++ wechat/controllers/ServerController.php
@@ -49,15 +49,16 @@ class ServerController extends BaseController {
 //            $resultStr = \Yii::$app->wechat->responseNews($fromUsername , $toUsername);
 
             /***********************客服消息****************************/
-            $articles = [
+            /*$articles = [
                 [
                     'title' => 'Happy Day',
                     'description' => 'Is Really A Happy Day',
                     'url' => '',
                     'picurl' => ''
                 ]
-            ];
-            $resultStr = \Yii::$app->wechat->sendNews($fromUsername , $articles);
+            ];*/
+//            $resultStr = \Yii::$app->wechat->sendNews($fromUsername , $articles);
+            $resultStr = \Yii::$app->wechat->sendText($fromUsername , '感谢您关注嘉瑞百合缘，请点击下方按钮“进入嘉瑞”，开启您的交友征婚之旅！');
             /***********************客服消息****************************/
 
             file_put_contents('./log.txt' , $resultStr."\n" ,FILE_APPEND);
