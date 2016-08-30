24b1adf31decbdec5d85c288035fe36f495d4598
499478785@qq.com <499478785@qq.com>
2016-08-30 10:58:29 +0800

    ts

--- wechat/web/js/app/controller/ioSocket.js
+++ wechat/web/js/app/controller/ioSocket.js
@@ -41,10 +41,9 @@ define(['app/module', 'app/directive/directiveApi'
         var list = ar.getStorage('chat_messageHistory' + $scope.receiveId);
         // 状态发送中的全部改为发送失败
         for (var i in list) {
-            if (list[i].send_user_id != userId && list[i].receive_user_id != userId ){
+            if (list[i].send_user_id != $scope.userId && list[i].receive_user_id != $scope.userId ){
                 list.splice(i,1);
-            }
-            if (list[i].status == 3) {
+            }else if (list[i].status == 3) {
                 list[i].status = 4;
             }
         }
