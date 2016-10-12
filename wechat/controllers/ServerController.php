64257c8f799911adf076a0b84737cfc8e0a9b781
Administrator <570434734@qq.com>
2016-10-12 16:25:53 +0800

    nsk

--- wechat/controllers/ServerController.php
+++ wechat/controllers/ServerController.php
@@ -62,7 +62,7 @@ class ServerController extends BaseController {
             [
                 'title' => '嘉瑞百合缘微站上线啦！',
                 'description' => '全新界面，简易操作，实名认证，安全放心！',
-                'url' => 'http://wechat.baihey.com/wap',
+                'url' => 'http://mp.weixin.qq.com/s?__biz=MzAxMDI2NzY2NQ==&mid=304474733&idx=1&sn=e2b09f2496883d0a15d1e4dd9a6a1a47&chksm=0c83b7133bf43e050bfee9c787ecf4d6a312e314e2fdd579cd60a6a73e3e6854c4c2a929353e&mpshare=1&scene=1&srcid=1012zMg7WQkn9IcDEkgkxd2l&from=singlemessage&isappinstalled=0#wechat_redirect',
                 'picurl' => 'https://mmbiz.qlogo.cn/mmbiz_jpg/hD1GpvgKwC7odZymQsyZ6MsxkBEB0X0s16k5ApWOYfHCXXUlsda4DwmicuuFEH6iaZCia5ZKnwet0vdUoxZ89I5Wg/0?wx_fmt=jpeg'
             ]
         ];
