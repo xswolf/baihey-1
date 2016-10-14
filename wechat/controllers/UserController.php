93eab5ac52ee22222c4f1c8294c9b2469414f4dd
499478785@qq.com <499478785@qq.com>
2016-10-14 15:53:29 +0800

    clean debug

--- wechat/controllers/UserController.php
+++ wechat/controllers/UserController.php
@@ -76,7 +76,7 @@ class UserController extends BaseController
         if (\Yii::$app->request->get('username') && \Yii::$app->request->get('password')) {
             if ($user = User::getInstance()->login($this->get['username'], $this->get['password'])) {
 
-                if ($user['wx_id'] == '' && $_COOKIE['wx_id'] != ''){
+                if ($user['wx_id'] == '' && Cookie::getInstance()->getCookie('wx_id') != ''){
                     User::getInstance()->editUser1(['id'=>$user['id'] , 'wx_id' => $_COOKIE['wx_id']]);
                 }
 
