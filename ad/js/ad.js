d5662349446f25ecf2c0ce7d693aea9c1be1142f
Administrator <570434734@qq.com>
2016-10-17 20:16:38 +0800

    nsk。

--- ad/js/ad.js
+++ ad/js/ad.js
@@ -1,3 +1,28 @@
-/**
- * Created by NSK. on 2016/10/17/0017.
- */
+$(function () {
+    $(".row .col-xs-3 img").each(function (index,ele) {
+        $(this).click(function (e) {
+            e.stopPropagation();
+            $(".show_pic_bg").show();
+            $(".show_pic_box").show();
+            $(".big_pic>img").attr('src', $(this).attr('src'));
+            $(".big_pic .pic_describe").text($(this).data('describe'));
+            $(".next_btn").data('index',$(this).data('index') + 1);
+        });
+    });
+    $(document).bind('click',function(e){
+        var target =$(e.target);
+        if(target.closest(".next_btn").length == 0){
+            $(".show_pic_box").hide();
+            $(".show_pic_bg").hide();
+        }
+    })
+    $(".next_btn").click(function(){
+        var idx = $(this).data('index');
+        if(idx == 13){
+            idx = 1;
+        }
+        $(".big_pic>img").attr('src', $("img[data-index='"+ idx + "']").attr('src'));
+        $(".big_pic .pic_describe").text($("img[data-index='"+ idx + "']").data('describe'));
+        $(".next_btn").data('index',idx + 1);
+    })
+});
